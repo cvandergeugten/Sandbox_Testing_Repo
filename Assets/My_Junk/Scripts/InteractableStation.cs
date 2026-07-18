928d87af84125ffae2d683ec9e96e8932d0912fa
Chance Vandergeugten <leaveitalltochance@gmail.com>
2026-07-18 10:33:56 -0700

    Yo mama

--- Assets/My_Junk/Scripts/InteractableStation.cs
+++ Assets/My_Junk/Scripts/InteractableStation.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 public abstract class InteractableStation : MonoBehaviour
 {
     public string stationName = "Station";
-    public float interactionRange = 2f;
+    [Range(0.5f, 10f)]
+    public float interactionRange = 3f;
+    [Range(0.1f, 10f)]
+    public float interactionResetDelay = 2f;
     public Color highlightColor = Color.yellow;
+    public Color interactionColor = Color.green;
 
     private Renderer[] renderers;
     private Color[] originalColors;
+    private GameObject actorBeingColored;
+    private Color[] actorOriginalColors;
+    private bool hasStoredActorColors;
 
     void Awake()
     {
@@ -46,8 +53,66 @@ public abstract class InteractableStation : MonoBehaviour
         return Vector3.Distance(transform.position, actor.transform.position) <= interactionRange;
     }
 
+    private void StoreActorColors(GameObject actor)
+    {
+        if (actor == null)
+            return;
+
+        Renderer[] actorRenderers = actor.GetComponentsInChildren<Renderer>();
+        actorOriginalColors = new Color[actorRenderers.Length];
+        for (int i = 0; i < actorRenderers.Length; i++)
+        {
+            actorOriginalColors[i] = actorRenderers[i].material.color;
+        }
+
+        actorBeingColored = actor;
+        hasStoredActorColors = true;
+    }
+
+    protected void ApplyInteractionColor(GameObject actor, Color color)
+    {
+        if (actor == null)
+            return;
+
+        if (!hasStoredActorColors || actorBeingColored != actor)
+        {
+            StoreActorColors(actor);
+        }
+
+        Renderer[] actorRenderers = actor.GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < actorRenderers.Length; i++)
+        {
+            actorRenderers[i].material.color = color;
+        }
+    }
+
+    private void ResetActorColor()
+    {
+        if (actorBeingColored == null || !hasStoredActorColors)
+            return;
+
+        Renderer[] actorRenderers = actorBeingColored.GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < actorRenderers.Length; i++)
+        {
+            if (i < actorOriginalColors.Length)
+            {
+                actorRenderers[i].material.color = actorOriginalColors[i];
+            }
+        }
+
+        actorBeingColored = null;
+        actorOriginalColors = new Color[0];
+        hasStoredActorColors = false;
+    }
+
     public virtual void Interact(GameObject actor)
     {
         Debug.Log(actor.name + " interacted with " + stationName);
+
+        if (actor != null && actor.CompareTag("Test_Dummy"))
+        {
+            ApplyInteractionColor(actor, interactionColor);
+            Invoke(nameof(ResetActorColor), interactionResetDelay);
+        }
     }
 }
